e7d0c98cb454374cf47585bda91d10bb2e2805a0
Kevin Carbonaro <kevin.carbonaro@gmail.com>
2019-10-19 14:47:48 +0200

    Storage Class
    Class responsible only for reading and writing data. Added function to read the data structure file and store it in a variable, ready to be parsed by the program. The data is in json format.

--- classes/storage.py
+++ classes/storage.py
@@ -1,5 +1,15 @@
 # This class is used to create an object enabling reading
 # and saving data to/from flat files
-class Storage
 
-    #
+import json
+
+class Storage:
+
+    def __init__(self, path_data_structure):
+        self.dataStructure = ""
+        self.pathDataStructure = path_data_structure
+
+    # Read data structure
+    def ReadDataStructure(self):
+        fileDataStructure = open(self.pathDataStructure)
+        self.dataStructure = fileDataStructure.read()
---
+++ main.py
@@ -0,0 +1,6 @@
+# This is the main application
+
+from classes.storage import Storage
+
+storage = Storage("data/data-structure.json")
+storage.ReadDataStructure()
